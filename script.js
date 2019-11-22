8f2b733ecd08b1a860868a895eda39b4d3cef446
Alba Olveros <x8a.018@gmail.com>
2019-11-22 18:15:45 +0100

    - Created the setPreviewColor function
    - Created the addBox function
    - Setting the preview color depending on the input
    - Making the add-to-favorite button work

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 /* Exercise 2: Color picker */
+$(document).on('keyup', '#color', function () {
+    setPreviewColor($('#color').val())
+   })
+
+$(document).on('click', '#add-to-favorite', function () {
+    addBox($('#color').val());
+    setPreviewColor('');
+    $('#color').focus().val('');
+})
+
+function setPreviewColor(color) {
+    //reading the rgb color code --> $('.preview').css('background-color');
+    $('.preview').css('background-color', color);
+}
+
+function addBox(color) {
+    $('#colors').prepend('<div class="item" style="background-color: ' + color + ';"></div>');
+}
