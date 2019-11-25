b6e6fd0c585bde26d447cfa1fb8a6d61a34b6d83
Alba Olveros <x8a.018@gmail.com>
2019-11-25 18:36:33 +0100

    Added:
    - Add a random color from my array in the preview area when the page is loaded
    - Add a color to favs when pressing the Enter key

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 /* Exercise 2: Color picker */
+var colors = ['A4A4A4', '088A29', '070719', 'FF0000', 'cc66ff'];
+var randomColor = Math.floor(Math.random() * colors.length);
+
 $(document).on('keyup', '#color', function () {
-    setPreviewColor($('#color').val())
-   })
+    setPreviewColor($('#color').val());
+   });
 
 $(document).on('click', '#add-to-favorite', function () {
     addBox($('#color').val());
     setPreviewColor('');
     $('#color').focus().val('');
-})
+});
+
+$(document).on('keydown', '#color', function (e) {
+    var key = e.which;
+    if (key == 13) {
+        addBox($('#color').val());
+        setPreviewColor('');
+        $('#color').focus().val('');
+    }
+   });
+   
+$(document).ready(function() {
+    setPreviewColor(colors[randomColor]);
+});
 
 function setPreviewColor(color) {
-    //reading the rgb color code --> $('.preview').css('background-color');
     $('.preview').css('background-color', color);
+    // not working --> $('.color-code').val($('.preview').css('background-color'));
 }
 
 function addBox(color) {
